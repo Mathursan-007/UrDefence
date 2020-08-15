0488ab9d9458eba89a2bbb683874800cc8df01f3
IT19125794 <it19125794@my.sliit.lk>
2020-08-15 20:42:23 +0530

    Dashboard news page design commit

--- app/src/main/java/com/example/mad/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mad/ui/home/HomeFragment.java
@@ -23,11 +23,25 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.textView16);
+        final TextView textView1 = root.findViewById(R.id.textView23);
+        final TextView textView2 = root.findViewById(R.id.textView30);
+        final TextView textView3 = root.findViewById(R.id.textView31);
+        final TextView textView4 = root.findViewById(R.id.textView32);
+        final TextView textView5 = root.findViewById(R.id.textView33);
+        final TextView textView6 = root.findViewById(R.id.textView34);
+        final TextView textView7 = root.findViewById(R.id.textView35);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+                textView1.setText(s);
+                textView2.setText(s);
+                textView3.setText(s);
+                textView4.setText(s);
+                textView5.setText(s);
+                textView6.setText(s);
+                textView7.setText(s);
             }
         });
         return root;
